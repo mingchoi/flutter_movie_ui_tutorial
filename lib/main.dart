1dbf03879ca478322c38d1294593770bd672f40e
Ming Choi <mingchoi.na@gmail.com>
2019-10-23 20:03:39 +0800

    Step 9 - Update data source & loading state

--- lib/main.dart
+++ lib/main.dart
@@ -11,24 +11,48 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final Movie movie = MovieRepo.getSampleMovie();
-
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        body: ListView(
-          children: <Widget>[
-            MovieHeader(movie: movie),
-            StoryDescription(movie: movie),
-            PhotoGallery(movie: movie),
-            ActorList(movie: movie),
-          ],
-        ),
-      ),
+      home: MovieInfoPage(),
     );
   }
 }
 
+class MovieInfoPage extends StatefulWidget {
+  const MovieInfoPage({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _MovieInfoPageState createState() => _MovieInfoPageState();
+}
+
+class _MovieInfoPageState extends State<MovieInfoPage> {
+  Movie _movie;
+
+  _MovieInfoPageState() {
+    MovieRepo repo = MovieRepo();
+    repo.getFirstMovieFake().listen((m) => setState(() {
+          _movie = m;
+        }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _movie == null
+          ? Center(child: CircularProgressIndicator())
+          : ListView(
+              children: <Widget>[
+                MovieHeader(movie: _movie),
+                StoryDescription(movie: _movie),
+                PhotoGallery(movie: _movie),
+                ActorList(movie: _movie),
+              ],
+            ),
+    );
+  }
+}
